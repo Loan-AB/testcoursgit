774d8c77f65c56860e63cc7e22805ce3490ed1b3
loanb <loanb@DESKTOP-GDOLRE8>
2018-03-09 11:33:33 +0100

    ajout du main :D

---
+++ Project_Java_raisin/src/Supertestdugit/test.java
@@ -0,0 +1,14 @@
+package Supertestdugit;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Testgit salut = new TestgitUpdate("OWNER CHABGE para");
+		
+		System.out.println(salut.retrunTheString());
+
+	}
+
+}
