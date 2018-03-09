6e8e93a41d7bbf65077e0e33403245547d3b994a
loanb <loanb@DESKTOP-GDOLRE8>
2018-03-09 11:30:48 +0100

    super

---
+++ Project_Java_raisin/src/Supertestdugit/Testgit.java
@@ -0,0 +1,20 @@
+package Supertestdugit;
+
+public class Testgit {
+
+	private String gitFromScratch;
+	
+	public Testgit() {
+		
+	}
+	
+	public Testgit(String para) {
+		this.gitFromScratch = para;
+		
+	}
+	
+	public String retrunTheString() {
+		return this.gitFromScratch;
+	}
+	
+}
---
+++ Project_Java_raisin/src/Supertestdugit/TestgitUpdate.java
@@ -0,0 +1,15 @@
+package Supertestdugit;
+
+public class TestgitUpdate extends Testgit {
+
+	//je suis une extension :D
+	public TestgitUpdate() 
+	{
+		
+	}
+	
+	public TestgitUpdate(String para) {
+		super(para);
+	}
+	
+}
